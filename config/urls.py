31f07d06a7289ed39e34313abe775de1507cc542
spuhaha18 <spuhaha18@gmail.com>
2026-05-02 21:38:41 +0900

    feat: add root URL redirecting to login or documents list based on auth state

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", auth_views.LoginView.as_view(redirect_authenticated_user=True), name="home"),
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("accounts/password_change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
