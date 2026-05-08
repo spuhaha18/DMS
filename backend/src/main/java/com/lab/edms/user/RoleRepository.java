b4861cd2465d9d7e9af90e654e4827fad51c7664
spuhaha18 <spuhaha18@gmail.com>
2026-05-08 16:34:01 +0900

    user: add Spring Data repositories

---
+++ backend/src/main/java/com/lab/edms/user/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.lab.edms.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRoleCode(String roleCode);
+}
